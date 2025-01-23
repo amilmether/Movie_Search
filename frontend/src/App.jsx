23b2ab34f45c4a1214d525ec214a00b85befd999
Amil <amilmether37@gmail.com>
2025-01-23 20:41:37 +0530

    page routing (navbar)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css';
 import Home from './pages/Home';
+import Favorite from './pages/Favorites';
+import NavBar from './components/NavBar';
+import { Routes,Route } from 'react-router-dom';
 function App() {
   const movienumber = 1;
   return (
     <>
-      <Home/>
+    <div>
+      <NavBar />
+    </div>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/favorites' element={<Favorite />} />
+      </Routes>
+    </main>
     </>
   );
 }
