f88c751d42fbbc3358d6c37f8281fa85c31495f6
Amil <amilmether37@gmail.com>
2025-01-23 15:29:35 +0530

    Conditional rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
 import './App.css'
-
+import MovieCard from './components/MovieCard'
 function App() {
+  const movienumber = 1;
   return (
     <>
-    <Text display="Facebook"/>
-    <Text display="Instagram"/>
+      {movienumber === 2 ? (
+        <MovieCard movie={{title: "Pusha The Rise",release_date: "2024" }}/>
+      ):(
+        <MovieCard movie={{title: "Lucifer",release_date: "2021" }}/>
+      )}
+      {movienumber === 1 && <MovieCard movie={{title: "Lucifer",release_date: "2021" }}/>}
     </>
-  )
+  );
 }
-function Text({display}) {
-  return (
-    <div>
-      <p>{display}</p>
-      <h1>hello world</h1>
-    </div>
-  )
-}
+
 export default App
